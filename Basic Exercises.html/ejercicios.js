76ffc4941cdc5badaaa8ea05a595ebba87f03aab
alejandrahidalgomi@gmail.com <you@example.com>
2024-02-16 09:12:29 -0300

    commit 1

---
+++ Basic Exercises.html/ejercicios.js
@@ -0,0 +1,113 @@
+
+
+
+let myName="Alejandra"
+let myLastName="Hidalgo"
+//const myAge=("45")
+const myPet="Gato"
+const petAge=("3")
+
+const nombreCompleto=("Alejandra Hidalgo")
+console.log(nombreCompleto)
+
+
+const PresentationText={
+    nombre:"Alejandra",
+    apellido:"Hidalgo",
+    edad:"45 años",
+    mascota:"gato",
+    edadDeLaMascota:"3 años"
+
+ }
+console.log(PresentationText)
+
+
+
+const estudiante={
+    nombre:"Carlos",
+    apellido:"Perez",
+    edad:25,
+    Genero:"M",
+    caracter:"introvertido"
+
+}
+
+console.table(["Carlos", "Perez", 25,"M",175]);
+
+console.log(estudiante,[2])
+
+const pet={
+    nombre:"kumal",
+    color:"rubio",
+    caracter:"jugueton",
+    edad:"4 años",
+    constectura:"mediano"
+
+}
+console.table(["kumal","rubio","jugueton","4 años","mediano"])
+console.log(estudiante,[0]) 
+
+let Frutas = ["banana", "manzana", "pera"];
+
+console.log(Frutas)
+console.log(Frutas[0])
+console.log(Frutas[1])
+console.log(Frutas[2])
+
+
+
+
+ let edad=prompt("Ingrese su edad")
+
+ let soyUnAdulto=Number(edad)>=18
+
+ if(soyUnAdulto){console.log("Soy un adulto")}
+
+let numeros = [0,2,4,6,8,10];
+
+console.log(numeros)
+
+console.log(numeros[0])
+console.log(numeros[1])
+console.log(numeros[2])
+console.log(numeros[3])
+console.log(numeros[4])
+console.log(numeros[5])
+
+const familia= [ {
+    nombre:"Alejandra",
+    apellido: "Hidalgo"
+  
+  },
+  {
+    nombre:"Javier",
+    apellido: "Piston"
+  
+  },
+  {
+      nombre:"Milagros",
+      apellido: "Piston"
+    
+    },
+    {
+      nombre:"Fernando",
+      apellido: "Piston"
+    
+    },
+    {
+      nombre:"Maria",
+      apellido: "Berg"
+    
+    },
+]
+console.log(familia)
+
+console.log(familia[0])
+console.log(familia[1])
+console.log(familia[2])
+console.log(familia[3])
+console.log(familia[4])
+
+ const randomText=console.log("manzana 6 Alejandra")
+
+
